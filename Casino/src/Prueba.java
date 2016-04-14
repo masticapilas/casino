4232e37f8a3a4b4abf1f4011f87de14e6278b0a8
alumno <alumno@A12110>
2016-04-14 13:29:44 +0200

    lñkzdfjkldjsfkljds

--- Casino/src/Prueba.java
+++ Casino/src/Prueba.java
@@ -24,13 +24,7 @@ public class Prueba {
      }
      
      public void cosa3(){
-         String pene;
-         for (int i; i < pene ; i++){
-             
-             
-             cacaue;
-             
-             
+
          }
          
      }
