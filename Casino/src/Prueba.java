e8919cb44d46a22ee26a58dc9ba515baded46b21
Foal <>
2016-04-14 13:43:57 +0200

    Limpiar Prueba

--- Casino/src/Prueba.java
+++ Casino/src/Prueba.java
@@ -1,6 +1,4 @@
 
-import javax.swing.JOptionPane;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,37 +11,6 @@ import java.util.*;
  * @author alumno
  */
 public class Prueba {
-    
-     private String hola;
-     
-     public void cosa(){
-         
-     }
-     
-     public void cosa2(){
-         
-     }
-     
-     public void cosa3(){
-
-         }
-         
-     }
-     
-     public void asdfasdfasdf(){
-         //cosa
-     }
-
-     public int pacoVenAClase(int paco){
-     
-         if(paco!=0){
-             JOptionPane.showMessageDialog(null, paco, "Ven ya cabron",paco );
-         }
-         
-         
-     return pacoooo;
-     }
-
-hola luis angel cabrooon
+    // Clase pa pruebas
      
 }
